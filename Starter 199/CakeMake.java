9e70062dd913d1b1f0dcbd59998907322acfcd5e
Dinkar Thakur <322dinkar@gmail.com>
2025-08-14 18:02:05 +0530

    starter199Q1

---
+++ Starter 199/CakeMake.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan=new Scanner(System.in);
+		int A=scan.nextInt();
+		int B =scan.nextInt();
+		int ans= (A*B)-Math.min(A,B);
+		System.out.print(ans);
+
+	}
+}
